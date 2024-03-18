aec62b31062c7416ac11795ad57d64a19872f7b8
KyungjinLee <bramhome5848@gmail.com>
2024-03-18 17:50:54 +0900

    20240318 섹션3. 데이터 접근 기술 - 테스트 - 임베디드 모드 DB

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,14 +1,15 @@
 package hello.itemservice;
 
-import hello.itemservice.config.*;
+import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-
+@Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
@@ -26,4 +27,21 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
+    /**
+     * 스프링 부트는 데이터베이스에 대한 별다른 설정이 없으면 임베디드 데이터베이스를 사용
+     * application.properties 에 있는 접근 설정 정보도 주석
+     -> 별다른 정보가 없으면 스프링 부트는 임베디드 모드로 접근하는 데이터소스(DataSource)를 만들어 제공
+     */
+//    @Bean
+//    @Profile("test")
+//    public DataSource dataSource() {
+//        log.info("메모리 데이터베이스 초기화");
+//
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.h2.Driver");
+//        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");  //메모리 모드로 동작, 켜넥션 연결이 모두 끊어지면 디비가 종료되는 것을 방지
+//        dataSource.setUsername("sa");
+//        dataSource.setPassword("");
+//        return dataSource;
+//    }
 }
